c67c559eefa977d81935abcbf4582ceed6e9a4d9
Husayn Hakeem <husaynhakeem@gmail.com>
2017-10-06 14:40:18 +0100

    Removed fake unit and instrumentation test, created a test for the PersonViewModel, added fake data generator class

---
+++ app/src/androidTest/java/husaynhakeem/io/kotlin_sample/FakeData.kt
@@ -0,0 +1,73 @@
+package husaynhakeem.io.kotlin_sample
+
+import husaynhakeem.io.kotlin_sample.model.*
+
+/**
+ * Created by husaynhakeem on 10/6/17.
+ */
+class FakeData {
+
+    companion object {
+
+        var ANY_GENDER = "some_gender"
+        var ANY_EMAIL = "some_email"
+        var ANY_BIRTHDAY = "some_birthday"
+        var ANY_REGISTRATION = "some_registration"
+        var ANY_PHONE = "some_phone"
+        var ANY_CELL = "some_cell"
+        var ANY_NAT = "some_nat"
+
+        fun ANY_PERSON() = Person(ANY_GENDER,
+                ANY_NAME(),
+                ANY_LOCATION(),
+                ANY_EMAIL,
+                ANY_LOGIN(),
+                ANY_BIRTHDAY,
+                ANY_REGISTRATION,
+                ANY_PHONE,
+                ANY_CELL,
+                ANY_ID(),
+                ANY_PICTURE(),
+                ANY_NAT)
+
+        var ANY_TITLE = "some_title"
+        var ANY_FIRST = "Husayn"
+        var ANY_LAST = "Hakeem"
+        fun ANY_NAME() = Name(ANY_TITLE,
+                ANY_FIRST,
+                ANY_LAST)
+
+        var ANY_STREET = "some_street"
+        var ANY_CITY = "some_city"
+        var ANY_STATE = "some_state"
+        var ANY_POSTCODE = 20000
+        fun ANY_LOCATION() = Location(ANY_STREET,
+                ANY_CITY,
+                ANY_STATE,
+                ANY_POSTCODE)
+
+        var ANY_USERNAME = "some_username"
+        var ANY_PASSWORD = "some_password"
+        var ANY_SALT = "some_salt"
+        var ANY_MD5 = "some_md5"
+        var ANY_SHA1 = "some_sha1"
+        var ANY_SHA256 = "some_sha256"
+        fun ANY_LOGIN() = Login(ANY_USERNAME,
+                ANY_PASSWORD,
+                ANY_SALT,
+                ANY_MD5,
+                ANY_SHA1,
+                ANY_SHA256)
+
+        var ANY_NAME = "some_name"
+        var ANY_VALUE = "some_value"
+        fun ANY_ID() = Id(ANY_NAME, ANY_VALUE)
+
+        var ANY_LARGE = "some_large"
+        var ANY_MEDIUM = "some_medium"
+        var ANY_THUMBNAIL = "some_thumbnail"
+        fun ANY_PICTURE() = Picture(ANY_LARGE,
+                ANY_MEDIUM,
+                ANY_THUMBNAIL)
+    }
+}
---
+++ app/src/androidTest/java/husaynhakeem/io/kotlin_sample/PersonViewModelShould.kt
@@ -0,0 +1,47 @@
+package husaynhakeem.io.kotlin_sample
+
+import android.support.test.InstrumentationRegistry
+import husaynhakeem.io.kotlin_sample.FakeData.Companion.ANY_PERSON
+import husaynhakeem.io.kotlin_sample.listing.item.PersonViewModel
+import junit.framework.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Created by husaynhakeem on 10/6/17.
+ */
+class PersonViewModelShould {
+
+    val context = InstrumentationRegistry.getTargetContext()
+    val person = ANY_PERSON()
+    lateinit var personViewModel: PersonViewModel
+
+    @Before
+    fun setUp() {
+        personViewModel = PersonViewModel(context, person)
+    }
+
+    @Test
+    fun returnCorrectFullName() {
+        val fullName = personViewModel.getFullName()
+        assertEquals("Some_title Husayn Hakeem ", fullName)
+    }
+
+    @Test
+    fun returnCorrectPicture() {
+        val picture = personViewModel.getPicture()
+        assertEquals("some_medium", picture)
+    }
+
+    @Test
+    fun returnCorrectEmail() {
+        val email = personViewModel.getEmail()
+        assertEquals("some_email", email)
+    }
+
+    @Test
+    fun returnCorrectPhone() {
+        val phone = personViewModel.getPhone()
+        assertEquals("some_phone", phone)
+    }
+}
